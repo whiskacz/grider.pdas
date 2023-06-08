faf1f9a6a1501b5f93b88c4f0565bd3d0d1d50e6
whiskacz <pawelblasiak1985@gmail.com>
2023-06-08 21:18:22 +0200

    grider.pdas

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import 'bulma/css/bulma.css'
+import { ProfileCard } from "./ProfileCard";
+import AlexaImage from "./images/alexa.png";
+import CortanaImage from "./images/cortana.png";
+import SiriImage from "./images/siri.png"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+function App () {
+    return (
+        
+    <div>
+        <section className="hero is-primary">
+            <div className="hero-body">
+                <p className="title">
+                Personal Digital Assistants
+                </p>
+            </div>
+        </section>
+        
+        <div className="container">
+            <section className="section">
+                <div className="columns">
+                    <div className="column is-4">
+                        <ProfileCard 
+                        title="Alexa" 
+                        handle="@alexa99" 
+                        age={35} 
+                        image={AlexaImage}
+                        description="something about Alexa"
+                        />
+                    </div>
+                    <div className="column is-4">
+                        <ProfileCard 
+                        title="Cortana" 
+                        handle="@cortana32" 
+                        age={22} 
+                        image={CortanaImage}
+                        description="something about Cortana"
+                        />    
+                    </div>
+                    <div className="column is-4">
+                    <ProfileCard 
+                    title="Siri" 
+                    handle="@siri01" 
+                    age={25} 
+                    image={SiriImage}
+                    description="something about Siri"
+                    />   
+                    </div>
+                </div>
+            </section>
+        </div>
+        
+        
+        
     </div>
-  );
-}
+)}
 
-export default App;
+export default App
